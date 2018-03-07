95a5c5d7c6724d667fe6f5b6990d8112ec95e4c2
Rover12421 <rover12421@163.com>
2018-03-07 23:07:23 +0800

    add xposed model

---
+++ error/src/main/java/android/content/res/Resources.java
@@ -0,0 +1,10 @@
+package android.content.res;
+
+import android.util.DisplayMetrics;
+
+public class Resources {
+    @Deprecated
+    public Resources(AssetManager assets, DisplayMetrics metrics, Configuration config) {
+        throw new RuntimeException("Stub!");
+    }
+}
---
+++ error/src/main/java/android/content/res/TypedArray.java
@@ -0,0 +1,12 @@
+package android.content.res;
+
+public class TypedArray {
+    /** Only for API stubs creation, DO NOT USE! */
+    /*package*/ TypedArray() {
+        throw new UnsupportedOperationException("STUB");
+    }
+
+    protected TypedArray(Resources resources, int[] data, int[] indices, int len) {
+        throw new UnsupportedOperationException("STUB");
+    }
+}
