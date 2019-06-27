1834bb5527f6364fd2e227b4dbb7c2fffde85f24
Rover12421 <rover12421@163.com>
2019-06-27 13:41:50 +0800

    Ver 2.x
    未完成

--- test/src/main/java/com/rover12421/android/hideapi/test/ExdentsActivity.java
+++
@@ -1,6 +0,0 @@
-package com.rover12421.android.hideapi.test;
-
-import android.app.Activity;
-
-public class ExdentsActivity extends Activity {
-}
--- test/src/main/kotlin/com/rover12421/android/hideapi/testkt/ExdentsActivityKt.kt
+++
@@ -1,5 +0,0 @@
-package com.rover12421.android.hideapi.testkt
-
-import android.app.Activity
-
-class ExdentsActivityKt : Activity()
